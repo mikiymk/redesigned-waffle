f8dcbc84ecdd217c5e329e0579de696c3f86389d
ymk <65121726+mikiymk@users.noreply.github.com>
2023-06-29 21:42:36 +0900

    Update parse-utils.ts

--- src/parse-utils.ts
+++ src/parse-utils.ts
@@ -1,3 +1,5 @@
+import { ParseReader, EOF, get } from "./parse-reader";
+
 type ResultSuccess<T> =
   readonly [isSuccess: true, value: T];
 
@@ -9,6 +11,10 @@ export type Result<T, E extends Error = Error> =
   | ResultFailure<E>;
 
 export type Parser<T> = (pr: ParseReader) => Result<T>;
+
+export const $const = (value: T) => (): ResultSuccess<T> => {
+  return [true, value];
+};
 
 export const $expect = (str: string) => (pr: ParseReader): Result<string> => {
   const readString = [];
@@ -27,7 +33,39 @@ export const $expect = (str: string) => (pr: ParseReader): Result<string> => {
 };
 
 export const $while = <T>(containParser: Perser<T>, endParser: Parser<void>, delimiterParser?: Parser<void>) => (pr: ParseReader): Result<T[]> => {
-  while (!endParser(pr)[0]) {
+  const result = [];
+  for (;;) {
+    const [contOk, cont] = containParser(pr);
+    if (!contOk) {
+      return [false, cont];
+    }
+
+    result.push(cont);
     
+    if (delimiterParser) {
+      const [delOk, v] = delimiterParser(pr);
+      
+      if (!delOk) {
+        return [false, v];
+      }
+    }
+    const [endOk, v] = endParser(pr);
+    
+    if (!endOk) {
+      return [false, v];
+    }
   }
+
+  return [true, result];
+};
+
+export const $switch = (...conditions: Parser<T>[]) => (pr: ParseReader): Result<T>=> {
+  for (const parser of conditions) {
+    const [ok, value] = parser(pr);
+    if (ok) {
+      return [true, value];
+    }
+  }
+
+  return [false, new Error("uncaught condition")];
 };
