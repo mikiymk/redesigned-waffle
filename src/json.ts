5693c9c218fbece0c39a5b722695523666a7c34b
ymk <65121726+mikiymk@users.noreply.github.com>
2023-06-30 00:14:51 +0900

    Update json.ts

--- src/json.ts
+++ src/json.ts
@@ -1,6 +1,21 @@
 import { ParseReader, EOF } from "./reader";
 import {  } from "./utils";
 
+type JsonNull = { lang: "json", type: "null", value: null };
+type JsonBoolean = { lang: "json", type: "boolean", value: boolean };
+type JsonNumber = { lang: "json", type: "number", value: number };
+type JsonString = { lang: "json", type: "string", value: string };
+type JsonArray = { lang: "json", type: "array", value: JsonValue[] };
+type JsonObjectEntry = { lang: "json", type: "object entry", value: [JsonString, JsonValue] };
+type JsonObject = { lang: "json", type: "object", value: JsonObjectEntry[] };
+type JsonValue =
+  | JsonNull
+  | JsonBoolean
+  | JsonNumber
+  | JsonString
+  | JsonArray
+  | JsonObject;
+
 const ws =
   $while($switch(
     $expect("\u0009"),
@@ -10,12 +25,12 @@ const ws =
   ));
 
 const jsonNull = 
-  $proc($expect("null"), () => ({ type: ["json", "null"] }));
+  $proc($expect("null"), () => ({ lang: "json", type: "null", value: null }));
 
 const jsonBoolean =
   $switch(
-    $expect("true"),
-    $expect("false"),
+    $proc($expect("true"), () => ({ lang: "json", type: "boolean", value: true })),
+    $proc($expect("false"), () => ({ lang: "json", type: "boolean", value: false })),
   );
 
 const jsonNumber =
