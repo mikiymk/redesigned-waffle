ed4315419341a302e06610cabc58a32a53ecd4f9
mikiymk <mikiymk001@gmail.com>
2023-07-15 17:16:09 +0900

    ２つの集合が互いに素かどうかの判定をする関数

--- lib/left-to-right-leftmost/generate-parser.ts
+++ lib/left-to-right-leftmost/generate-parser.ts
@@ -1,13 +1,26 @@
+import { getDirectorSetList } from "./director-set";
 import { getFirstSetList } from "./first-set";
+import { getFollowSetList } from "./follow-set";
+import { isDisjoint } from "./is-disjoint";
 
 import type { Syntax } from "./define-rules";
-
 /**
  *
  * @param syntax
  */
 export const generateParser = (syntax: Syntax) => {
-  const firstSets = getFirstSetList(syntax);
+  const firstSetList = getFirstSetList(syntax);
+  const followSetList = getFollowSetList(syntax, firstSetList);
+  const directorSetList = getDirectorSetList(firstSetList, followSetList);
 
-  console.log(firstSets);
+  for (const left of directorSetList)
+    for (const right of directorSetList) {
+      if (left === right) continue;
+
+      if (!isDisjoint(left, right)) {
+        console.error(`left and right is not disjoint`);
+      }
+    }
+
+  console.log(firstSetList, followSetList, directorSetList);
 };
---
+++ lib/left-to-right-leftmost/is-disjoint.ts
@@ -0,0 +1,51 @@
+import type { TokenSet } from "./token-set";
+
+/**
+ * ２つのトークン集合が最初の１文字について互いに素かどうかを判定する。
+ * @param left A
+ * @param right B
+ * @returns 互いに素なら `true`
+ */
+export const isDisjoint = (left: TokenSet, right: TokenSet): boolean => {
+  const leftFirstChars = firstChars(left);
+  const rightFirstChars = firstChars(right);
+
+  // 左辺の各文字についてループ
+  for (const [leftCharStart, leftCharEnd] of leftFirstChars) {
+    // 右辺の文字にかぶるところがあるかチェックする
+
+    if (
+      rightFirstChars.some(
+        ([rightCharStart, rightCharEnd]) => leftCharEnd < rightCharStart || rightCharEnd < leftCharStart,
+      )
+    ) {
+      return false;
+    }
+    // あったらfalseを返す
+  }
+
+  return true;
+};
+
+/**
+ * トークンの１文字目のリストをあげる
+ * @param tokens トークン集合
+ * @returns それぞれのトークンの１文字目の範囲
+ */
+const firstChars = (tokens: TokenSet): [number, number][] => {
+  const firstChars: [number, number][] = [];
+
+  for (const token of tokens) {
+    if (token[0] === "word") {
+      // eslint-disable-next-line unicorn/prefer-code-point
+      const char = token[1].charCodeAt(0);
+      firstChars.push([char, char]);
+    } else if (token[0] === "char") {
+      firstChars.push([token[1], token[2]]);
+    }
+  }
+
+  firstChars.sort(([a], [b]) => a - b);
+
+  return firstChars;
+};
