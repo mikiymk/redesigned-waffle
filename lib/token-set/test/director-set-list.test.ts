8605e9ef86fc6b22c8b1a448709c489d0bf4f355
mikiymk <mikiymk001@gmail.com>
2023-07-23 16:31:48 +0900

    Director集合のテスト

--- lib/token-set/test/director-set-list.test.ts
+++ lib/token-set/test/director-set-list.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { expect, test } from "vitest";
 
 import { char, eof, empty, reference, rule, word } from "@/lib/rules/define-rules";
 
@@ -7,47 +7,162 @@ import { getFirstSetList } from "../first-set-list";
 import { getFollowSetList } from "../follow-set-list";
 import { TokenSet } from "../token-set";
 
-describe("get first-set from syntax", () => {
-  test("basic token", () => {
-    const syntax = [
-      rule("start", reference("basic token"), word("after basic token")),
+test("１つの記号", () => {
+  const syntax = [
+    // start
+    rule("S", reference("E")),
 
-      rule("basic token", word("word"), word("after word")),
-      rule("basic token", char("A", "Z"), word("after char")),
-      rule("basic token", empty),
-    ];
+    // reference
+    rule("E", reference("B")),
 
-    const firstSet = getFirstSetList(syntax);
+    // terminal
+    rule("B", word("word")),
+    rule("B", char("a", "z")),
+    rule("B", empty),
+  ];
 
-    const expectedFirstSet = [
-      new TokenSet([word("word"), char("A", "Z"), word("after basic token")]),
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
 
-      new TokenSet([word("word")]),
-      new TokenSet([char("A", "Z")]),
-      new TokenSet([empty]),
-    ];
-    expect(firstSet).toStrictEqual(expectedFirstSet);
+  expect(result).toHaveLength(5);
+  expect(result[0]).toStrictEqual(new TokenSet([word("word"), char("a", "z"), eof]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("word"), char("a", "z"), eof]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word")]));
+  expect(result[3]).toStrictEqual(new TokenSet([char("a", "z")]));
+  expect(result[4]).toStrictEqual(new TokenSet([eof]));
+});
 
-    const followSet = getFollowSetList(syntax, firstSet);
-    const expectedFollowSet = [
-      new TokenSet([eof]),
+test("非終端記号の後に終端記号", () => {
+  const syntax = [
+    // start
+    rule("S", reference("E")),
 
-      new TokenSet([word("after basic token")]),
-      new TokenSet([word("after basic token")]),
-      new TokenSet([word("after basic token")]),
-    ];
+    // reference
+    rule("E", reference("B"), word("after defined")),
 
-    expect(followSet).toStrictEqual(expectedFollowSet);
+    // terminal
+    rule("B", word("word")),
+  ];
 
-    const result = getDirectorSetList(firstSet, followSet);
-    const expected = [
-      new TokenSet([word("word"), char("A", "Z"), word("after basic token")]),
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
 
-      new TokenSet([word("word")]),
-      new TokenSet([char("A", "Z")]),
-      new TokenSet([word("after basic token")]),
-    ];
+  expect(result).toHaveLength(3);
+  expect(result[0]).toStrictEqual(new TokenSet([word("word")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("word")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word")]));
+});
 
-    expect(result).toStrictEqual(expected);
-  });
+test("空になる可能性がある非終端記号の後に終端記号", () => {
+  const syntax = [
+    // start
+    rule("S", reference("E")),
+
+    // reference
+    rule("E", reference("B"), word("after defined")),
+
+    // terminal
+    rule("B", word("word")),
+    rule("B", empty),
+  ];
+
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
+
+  expect(result).toHaveLength(4);
+  expect(result[0]).toStrictEqual(new TokenSet([word("word"), word("after defined")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("word"), word("after defined")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word")]));
+  expect(result[3]).toStrictEqual(new TokenSet([word("after defined")]));
+});
+
+test("左再帰", () => {
+  const syntax = [
+    // start
+    rule("S", reference("E")),
+
+    // recursion
+    rule("E", reference("E"), word("follow lr")),
+    rule("E", word("word lr")),
+  ];
+
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
+
+  expect(result).toHaveLength(3);
+  expect(result[0]).toStrictEqual(new TokenSet([word("word lr")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("word lr")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word lr")]));
+});
+
+test("右再帰", () => {
+  const syntax = [
+    // start
+    rule("S", reference("E")),
+
+    // recursion
+    rule("E", word("lead rr"), reference("E")),
+    rule("E", word("word rr")),
+  ];
+
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
+
+  expect(result).toHaveLength(3);
+  expect(result[0]).toStrictEqual(new TokenSet([word("lead rr"), word("word rr")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("lead rr")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word rr")]));
+});
+
+test("間接の左再帰", () => {
+  const syntax = [
+    // start
+    rule("S", reference("A")),
+
+    // recursion 1
+    rule("A", reference("B"), word("follow in-lr 1")),
+
+    // recursion 2
+    rule("B", reference("A"), word("follow in-lr 2")),
+    rule("B", word("word in-lr")),
+  ];
+
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
+
+  expect(result).toHaveLength(4);
+  expect(result[0]).toStrictEqual(new TokenSet([word("word in-lr")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("word in-lr")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("word in-lr")]));
+  expect(result[3]).toStrictEqual(new TokenSet([word("word in-lr")]));
+});
+
+test("間接の右再帰", () => {
+  const syntax = [
+    // start
+    rule("S", reference("A")),
+
+    // recursion 1
+    rule("A", word("lead in-rr 1"), reference("B")),
+
+    // recursion 2
+    rule("B", word("lead in-rr 2"), reference("A")),
+    rule("B", word("word in-rr")),
+  ];
+
+  const firstSet = getFirstSetList(syntax);
+  const followSet = getFollowSetList(syntax, firstSet);
+  const result = getDirectorSetList(firstSet, followSet);
+
+  expect(result).toHaveLength(4);
+  expect(result[0]).toStrictEqual(new TokenSet([word("lead in-rr 1")]));
+  expect(result[1]).toStrictEqual(new TokenSet([word("lead in-rr 1")]));
+  expect(result[2]).toStrictEqual(new TokenSet([word("lead in-rr 2")]));
+  expect(result[3]).toStrictEqual(new TokenSet([word("word in-rr")]));
 });
