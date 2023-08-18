55b27d6169a58cd58330909ad7102374050e3546
mikiymk <mikiymk001@gmail.com>
2023-08-18 14:23:52 +0900

    構文解析表を作成する前に左側にない非終端記号を使用することを検査する

--- lib/left-to-right-rightmost/generate-parser.ts
+++ lib/left-to-right-rightmost/generate-parser.ts
@@ -1,4 +1,7 @@
+import { getRuleNames } from "../left-to-right-leftmost/rule-names";
 import { LRParser } from "../parser/lr-1";
+import { ReferenceToken } from "../rules/reference-token";
+import { primitiveToString } from "../util/primitive-to-string";
 
 import { generateParseTable } from "./parse-table";
 
@@ -10,6 +13,16 @@ import type { Syntax } from "../rules/define-rules";
  * @returns パーサー
  */
 export const generateParser = <T>(syntax: Syntax<T>) => {
+  // ルール名があるかを検査する
+  const ruleNames = getRuleNames(syntax);
+  for (const { tokens } of syntax) {
+    for (const token of tokens) {
+      if (token instanceof ReferenceToken && !ruleNames.includes(token.name)) {
+        throw new Error(`存在しないルール名を参照しています。 ${primitiveToString(token.name)}`);
+      }
+    }
+  }
+
   const table = generateParseTable(syntax);
 
   return new LRParser(syntax, table);
