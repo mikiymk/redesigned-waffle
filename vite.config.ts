eff16c0dd59de79fd6a443f15ec1096cef321441
mikiymk <mikiymk001@gmail.com>
2024-05-23 15:23:24 +0900

    fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { default as merge } from "deepmerge";
 import { defineConfig } from "vite";
-
-import type { UserConfig } from "vite";
 
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
@@ -10,37 +7,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // eslint-disable-next-line import/no-unused-modules
-export default defineConfig(({ mode }) => {
-  const common: UserConfig = {
-    base: "/ts-page-template/",
+export default defineConfig({
+  base: "/ts-page-template/",
 
-    plugins: [],
+  plugins: [],
 
-    resolve: {
-      alias: [{ find: /^@\/(.*)/, replacement: resolve(__dirname, "$1") }],
+  resolve: {
+    alias: [{ find: /^@\/(.*)/, replacement: resolve(__dirname, "$1") }],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "TsPageTemplate",
+      fileName: "template",
+      formats: ["es", "cjs"],
     },
-  };
-
-  if (mode === "lib") {
-    const libraryConfig: UserConfig = {
-      build: {
-        lib: {
-          entry: resolve(__dirname, "lib/main.ts"),
-          name: "TsPageTemplate",
-          fileName: "template",
-          formats: ["es", "cjs"],
-        },
-      },
-    };
-
-    return merge(common, libraryConfig);
-  }
-
-  const pagesConfig: UserConfig = {
-    build: {
-      outDir: "pages",
-    },
-  };
-
-  return merge(common, pagesConfig);
+  },
 });
